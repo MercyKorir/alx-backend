5868bd5fce06cdc76cb6d6ed5f3954d8f54d91bc
MercyKorir <mcthegreat78@gmail.com>
2023-07-26 22:05:52 +0300

    I have added the file for task 0*

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,7 +11,7 @@ class BasicCache(BaseCaching):
 
         if key is not None and item is not None:
             self.cache_data[key] = item
-    
+
     def get(self, key):
         """Returns value linked to key"""
 
