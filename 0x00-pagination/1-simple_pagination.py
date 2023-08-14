a1d9afe2d4a34870faa2b9012af9d77bf2cfd534
MercyKorir <mcthegreat78@gmail.com>
2023-08-14 12:56:15 +0300

    I have added the file for task 1*

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -13,6 +13,7 @@ def index_range(page: int, page_size: int) -> tuple:
 
     return (first_idx, last_idx)
 
+
 class Server:
     """
     Server class to paginate a database of popular baby names
@@ -34,8 +35,10 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Takes two args and returns a list of lists"""
-        assert isinstance(page, int) and isinstance(page_size, int), "Both arguments must be integers"
-        assert page > 0 and page_size > 0, "Both argment must be greater than zero"
+        inst_page = isinstance(page, int)
+        inst_page_size = isinstance(page_size, int)
+        assert inst_page and inst_page_size, "Must be integers"
+        assert page > 0 and page_size > 0, "Must be positive val"
         first_idx, last_idx = index_range(page, page_size)
         if first_idx >= len(self.dataset()):
             return []
